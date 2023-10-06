da12a0b85f6c3db405194a3dc93e9dffc5d866ba
Luc HOSTEIN <61002107+luc-hostein@users.noreply.github.com>
2023-10-06 09:43:56 +0200

    Push affichage weather
    Recupere la ville, sa météo et l'affiche

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,64 @@
 import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function GetWeather(props){
+  
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setWeather] = useState([])
+  const [cityToSearch, setCityToSearch] = useState("")
+  const [temp, setTemp] = useState("")
+
+  const fetchCityData = () => {
+    fetch("https://jb03dcnv74.execute-api.eu-west-1.amazonaws.com/Prod/weather/"+cityToSearch)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setWeather(data.condition);
+        setTemp(data.temperature);
+      });
+  };
+
+  const fetchLocalisation = () => {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(
+        function(position){
+          console.log(position);
+          var latitude = position.coords.latitude,
+          longitude = position.coords.longitudes;
+          console.log(latitude+" "+longitude);
+        },
+        (err) => console.log(err)
+      );
+    }
+
+    fetch("https://jb03dcnv74.execute-api.eu-west-1.amazonaws.com/Prod/geo?lon="+longitude+"&lat="+latitude)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  const onSubmitHandler = (event) =>{
+      event.preventDefault();
+      setCityToSearch(event.currentTarget.elements.city.value);
+      fetchCityData();
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={onSubmitHandler}><button type="submit"></button><input type='text' id='city'></input></form><br/>
+        <div>{cityToSearch}  {temp}</div><br/><div>{weather}</div>
+        <br/><button onClick={fetchLocalisation}>Geoloc</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          counts test is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
